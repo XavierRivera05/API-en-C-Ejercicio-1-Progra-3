cb340a83c16e5e99fc7c19e35cfefaf126725cda
XavierRivera05 <126819995+XavierRivera05@users.noreply.github.com>
2025-02-09 19:15:21 -0600

    nose

--- API con base de datos/API con base de datos/obj/Debug/net8.0/API con base de datos.AssemblyInfo.cs
+++ API con base de datos/API con base de datos/obj/Debug/net8.0/API con base de datos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API con base de datos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eb29dc79656325824554680335118b6b7d3d396")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+978ce7cbdddb1cf875b116131dda03649670b13d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API con base de datos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API con base de datos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
